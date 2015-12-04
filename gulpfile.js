d51a285bb7868ed12e1bed5255ffcf9d20ac73a8
William Teder <wteder@hydreon.com>
2015-12-03 18:37:11 -0600

    Remove unused dependencies, gulp & minification etc

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+	minifyCss = require('gulp-minify-css'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
+
+gulp.task('minify-css', function() {
+	return gulp.src('css/**/*.css')
+		.pipe(minifyCss({compatibility: 'ie8'}))
+		.pipe(concat('totem.vendor.min.css'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-js', function() {
+	return gulp.src('js/**/*.js')
+		.pipe(uglify())
+		.pipe(concat('totem.vendor.min.js'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['minify-css', 'minify-js'], function() {
+
+});
