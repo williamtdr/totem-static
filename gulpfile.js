93973e8409a4121b081c1e02a9240934113a74e7
William Teder <wteder@hydreon.com>
2015-12-09 15:41:34 -0600

    File upload capability

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,19 @@ gulp.task('minify-css', function() {
 });
 
 gulp.task('minify-js', function() {
-	return gulp.src(['js/jquery.min.js','js/jquery.noty.packaged.min.js', 'js/**/*.js'])
-		.pipe(uglify())
+	return gulp.src(['js/jquery.min.js', 'js/background.min.js', 'js/transition.min.js', 'js/**/*.js'])
+        .pipe(uglify())
 		.pipe(concat('totem.vendor.min.js'))
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['minify-css', 'minify-js'], function() {
+gulp.task('minify-file-upload', function() {
+    return gulp.src(['fileupload/jquery.ui.widget.js', 'fileupload/load-image.all.min.js', 'fileupload/canvas-to-blob.min.js', 'fileupload/jquery.fileupload.js', 'fileupload/jquery.fileupload-process.js', 'fileupload/jquery.fileupload-image.js', 'fileupload/jquery.fileupload-validate.js', 'fileupload/oncomplete.js'])
+        .pipe(uglify())
+        .pipe(concat('totem.fileupload.min.js'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ['minify-css', 'minify-js', 'minify-file-upload'], function() {
 
 });
