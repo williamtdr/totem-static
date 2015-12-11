ed7bafc750c2cc446e8c6fabaa9d8b290c9f96b7
William Teder <wteder@hydreon.com>
2015-12-10 20:10:30 -0600

    Custom backgrounds m9

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('minify-css', function() {
 });
 
 gulp.task('minify-js', function() {
-	return gulp.src(['js/jquery.min.js', 'js/background.min.js', 'js/transition.min.js', 'js/**/*.js'])
+	return gulp.src(['js/jquery.min.js', 'js/transition.min.js', 'js/**/*.js'])
         .pipe(uglify())
 		.pipe(concat('totem.vendor.min.js'))
 		.pipe(gulp.dest('dist'));
